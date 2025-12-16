bbb97de38d42f1d1bd2438747f574a5cd90bb231
bala-amypo <bala@amypo.in>
2025-12-16 10:13:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/AssignmentEvaluation.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+public class AssignmentEvaluation{
+    private long id;
+    private long taskId;
+    private long volunteerId;
+    private LocalDateTime assignedAt;
+    private String status;
+    private String notes;
+}
+public AssignmentEvaluation(){
+
+}
+
