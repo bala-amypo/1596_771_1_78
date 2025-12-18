a86be1699dbcc8cf4ef7f70c6406c7b18e97642a
bala-amypo <bala@amypo.in>
2025-12-18 16:17:51 +0000

    User commit

--- src/main/java/com/example/demo/model/TaskAssignmentRecord.java
+++ src/main/java/com/example/demo/model/TaskAssignmentRecord.java
@@ -10,21 +10,22 @@ public class TaskAssignmentRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String taskName;
+    private Long volunteerId;
 
-    @Column(nullable = false)
-    private String assignedTo;
+    private String taskName;
 
     public TaskAssignmentRecord() {}
 
-    public TaskAssignmentRecord(String taskName, String assignedTo) {
-        this.taskName = taskName;
-        this.assignedTo = assignedTo;
-    }
-
     public Long getId() {
         return id;
+    }
+
+    public Long getVolunteerId() {
+        return volunteerId;
+    }
+
+    public void setVolunteerId(Long volunteerId) {
+        this.volunteerId = volunteerId;
     }
 
     public String getTaskName() {
@@ -33,13 +34,5 @@ public class TaskAssignmentRecord {
 
     public void setTaskName(String taskName) {
         this.taskName = taskName;
-    }
-
-    public String getAssignedTo() {
-        return assignedTo;
-    }
-
-    public void setAssignedTo(String assignedTo) {
-        this.assignedTo = assignedTo;
     }
 }
