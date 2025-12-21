ca6bb4f7bc311745f6822c5db6e685186cc48fe2
bala-amypo <bala@amypo.in>
2025-12-21 10:04:17 +0000

    User commit

--- src/main/java/com/example/demo/service/AssignmentEvaluationService.java
+++ src/main/java/com/example/demo/service/AssignmentEvaluationService.java
@@ -1,11 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.model.AssignmentEvaluationRecord;
-import java.util.List;
-
+import java.util.Lisd'qwf'
+qfqe[
 public interface AssignmentEvaluationService {
 
     AssignmentEvaluationRecord evaluateAssignment(AssignmentEvaluationRecord evaluation);
     List<AssignmentEvaluationRecord> getEvaluationsByAssignment(Long assignmentId);
-    List<AssignmentEvaluationRecord> getAllEvaluations();
-}
+    List<AssignmentEvaluationRecord> getAllEvaluations(
