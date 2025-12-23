fdb3e6960a05ca5d248cec0f55f3822369474106
bala-amypo <bala@amypo.in>
2025-12-23 09:22:03 +0000

    User commit

--- src/main/java/com/example/demo/model/AssignmentEvaluationRecord.java
+++ src/main/java/com/example/demo/model/AssignmentEvaluationRecord.java
@@ -5,13 +5,6 @@ import java.time.LocalDateTime;
 
 @Entity
 public class AssignmentEvaluationRecord {
-//@Entity
-//public class AssignmentEvaluationRecord {
-
-   // @Id
-   //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "assignment_eval_seq")
-    //@SequenceGenerator(name = "assignment_eval_seq", sequenceName = "assignment_evaluation_record_seq", allocationSize = 1)
-    //private Long id;
 
     @Id
     @GeneratedValue
