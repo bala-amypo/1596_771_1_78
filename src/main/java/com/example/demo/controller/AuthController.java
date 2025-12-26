bb63f6895876c972d4b6b4e2a19597197ab605a4
bala-amypo <bala@amypo.in>
2025-12-26 07:56:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,24 +1,56 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.User;
-import com.example.demo.security.JwtUtil;
+import com.example.demo.security.CustomUserDetailsService;
+import com.example.demo.security.JwtTokenProvider;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.RegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
-    private final JwtUtil jwtUtil;
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
-    public AuthController(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @PostMapping("/register")
-    public String register(@RequestBody User user) {
-        return jwtUtil.generateToken(
-                user.getUsername(),
-                user.getUserId(),
-                user.getRole()
+    public Map<String, Object> register(@RequestBody RegisterRequest request) {
+        return userDetailsService.registerUser(
+                request.getFullName(),
+                request.getEmail(),
+                encoder.encode(request.getPassword()),
+                request.getRole()
         );
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword(),
+                Collections.emptyList()
+        );
+
+        String token = jwtTokenProvider.generateToken(
+                auth,
+                1L,
+                "USER"
+        );
+
+        return new AuthResponse(token);
     }
 }
