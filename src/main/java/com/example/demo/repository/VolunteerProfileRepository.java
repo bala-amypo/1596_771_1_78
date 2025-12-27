43be66d6910984b051593276304364d21660a33a
bala-amypo <bala@amypo.in>
2025-12-27 01:42:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/VolunteerProfileRepository.java
+++ src/main/java/com/example/demo/repository/VolunteerProfileRepository.java
@@ -1,20 +1,34 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
 
+// import com.example.demo.model.VolunteerProfile;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.List;
+// import java.util.Optional;
+
+// public interface VolunteerProfileRepository extends JpaRepository<VolunteerProfile, Long> {
+
+//     boolean existsByVolunteerId(String volunteerId);
+
+//     boolean existsByEmail(String email);
+
+//     boolean existsByPhone(String phone);
+
+//     Optional<VolunteerProfile> findByVolunteerId(String volunteerId);
+
+//     List<VolunteerProfile> findByAvailabilityStatus(String availabilityStatus);
+// }
+
+package com.example.demo.repository;
 import com.example.demo.model.VolunteerProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface VolunteerProfileRepository extends JpaRepository<VolunteerProfile, Long> {
-
-    boolean existsByVolunteerId(String volunteerId);
-
-    boolean existsByEmail(String email);
-
-    boolean existsByPhone(String phone);
-
-    Optional<VolunteerProfile> findByVolunteerId(String volunteerId);
-
-    List<VolunteerProfile> findByAvailabilityStatus(String availabilityStatus);
-}
+List<VolunteerProfile> findByAvailabilityStatus(String status);
+boolean existsByVolunteerId(String volunteerId);
+boolean existsByEmail(String email);
+boolean existsByPhone(String phone);
+Optional<VolunteerProfile> findByVolunteerId(String volunteerId);
