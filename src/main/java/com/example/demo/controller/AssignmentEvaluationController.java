3e577074c73c37ed9fbc3826324c79481c60f64f
bala-amypo <bala@amypo.in>
2025-12-27 15:08:56 +0000

    User commit

--- src/main/java/com/example/demo/controller/AssignmentEvaluationController.java
+++ src/main/java/com/example/demo/controller/AssignmentEvaluationController.java
@@ -5,6 +5,7 @@ import com.example.demo.service.AssignmentEvaluationService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/evaluations")
