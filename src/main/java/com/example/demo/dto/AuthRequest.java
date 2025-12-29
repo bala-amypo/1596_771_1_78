aea11fa639f8fd91f9a73172fb848c9460f22495
bala-amypo <bala@amypo.in>
2025-12-29 06:22:26 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,35 +1,66 @@
+// package com.example.demo.dto;
+
+// public class AuthRequest {
+
+//     private String username;
+//     private String password;
+//     private String role;
+
+//     public AuthRequest() {
+//     }
+
+//     public String getUsername() {
+//         return username;
+//     }
+
+//     public void setUsername(String username) {
+//         this.username = username;
+//     }
+
+//     public String getPassword() {
+//         return password;
+//     }
+    
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
+
+//     public String getRole() {
+//         return role;
+//     }
+    
+//     public void setRole(String role) {
+//         this.role = role;
+//     }
+// }
+
 package com.example.demo.dto;
 
 public class AuthRequest {
 
-    private String username;
+    private String email;
     private String password;
-    private String role;
 
-    public AuthRequest() {
+    public AuthRequest() {}
+
+    public AuthRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-    
-    public void setRole(String role) {
-        this.role = role;
     }
 }
