91867189009e230fa21e67fefc340819a80951a8
bala-amypo <bala@amypo.in>
2025-12-29 08:47:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/TaskAssignmentRecordRepository.java
+++ src/main/java/com/example/demo/repository/TaskAssignmentRecordRepository.java
@@ -1,30 +1,11 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.model.TaskAssignmentRecord;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import java.util.List;
-
-// public interface TaskAssignmentRecordRepository
-//         extends JpaRepository<TaskAssignmentRecord, Long> {
-
-//     boolean existsByTaskIdAndStatus(Long taskId, String status);
-
-//     List<TaskAssignmentRecord> findByTaskId(Long taskId);
-
-//     List<TaskAssignmentRecord> findByVolunteerId(Long volunteerId);
-// }
-
 package com.example.demo.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.TaskAssignmentRecord;
 
-public interface TaskAssignmentRecordRepository 
+public interface TaskAssignmentRecordRepository
         extends JpaRepository<TaskAssignmentRecord, Long> {
 
-    // ✅ CORRECT
     List<TaskAssignmentRecord> findByVolunteerProfile_Id(Long id);
 }
