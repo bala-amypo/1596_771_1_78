885ffd6669b986827d5b50ddebed7b9478c60b4e
bala-amypo <bala@amypo.in>
2025-12-29 06:05:28 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,56 +1,20 @@
-// // package com.example.demo.config;
+// package com.example.demo.config;
 
-// // import io.swagger.v3.oas.models.OpenAPI;
-// // import io.swagger.v3.oas.models.servers.Server;
-// // import org.springframework.context.annotation.Bean;
-// // import org.springframework.context.annotation.Configuration;
-// // import java.util.List;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.servers.Server;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import java.util.List;
 
-// // @Configuration
-// // public class SwaggerConfig {
+// @Configuration
+// public class SwaggerConfig {
 
-// //     @Bean
-// //     public OpenAPI customOpenAPI() {
-// //         return new OpenAPI()
-// //                 // You need to change the port as per your server
-// //                 .servers(List.of(
-// //                         new Server().url("https://9120.pro604cr.amypo.ai")
-// //                 ));
-// //         }
-// // }
-
-package com.example.demo.config;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("JWT Demo API")
-                        .version("1.0")
-                        .description("Simple JWT Demo Project for Students"))
-                .servers(List.of(
-                        new Server().url("https://9120.pro604cr.amypo.ai")
-                ))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                        .description("Enter JWT token")));
-    }
-}
-
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 // You need to change the port as per your server
+//                 .servers(List.of(
+//                         new Server().url("https://9120.pro604cr.amypo.ai")
+//                 ));
+//         }
+// }
