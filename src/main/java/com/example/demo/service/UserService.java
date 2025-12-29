4d4a43b4b4e961b92bfd8e4b8cf04ad70fa7096a
bala-amypo <bala@amypo.in>
2025-12-29 06:28:27 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,9 +1,22 @@
+// package com.example.demo.service;
+
+// import com.example.demo.model.User;
+// import org.springframework.security.core.Authentication;
+
+// public interface UserService {
+//     String registerUser(User user);
+//     String loginUser(String username, String password);
+// }
+
 package com.example.demo.service;
 
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
 import com.example.demo.model.User;
-import org.springframework.security.core.Authentication;
 
 public interface UserService {
-    String registerUser(User user);
-    String loginUser(String username, String password);
+    AuthResponse register(RegisterRequest request);
+    AuthResponse login(AuthRequest request);
+    User findByUsername(String username);
 }
