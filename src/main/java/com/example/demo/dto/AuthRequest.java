383350812f162d07092d38b0da0fddcd1ed25a13
bala-amypo <bala@amypo.in>
2025-12-29 07:11:59 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -33,27 +33,19 @@
 //         this.role = role;
 //     }
 // }
-
 package com.example.demo.dto;
 
 public class AuthRequest {
 
-    private String email;
+    private String username;
     private String password;
 
-    public AuthRequest() {}
-
-    public AuthRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public String getUsername() {
+        return username;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -29,39 +29,20 @@
 //         this.role = role;
 //     }
 // }
-
 package com.example.demo.dto;
 
 public class RegisterRequest {
 
-    private String fullName;
-    private String email;
+    private String username;
     private String password;
-    private String role; // Optional: VOLUNTEER / COORDINATOR
+    private String email;
 
-    public RegisterRequest() {}
-
-    public RegisterRequest(String fullName, String email, String password, String role) {
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
+    public String getUsername() {
+        return username;
     }
 
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -72,12 +53,11 @@ public class RegisterRequest {
         this.password = password;
     }
 
-    public String getRole() {
-        return role;
+    public String getEmail() {
+        return email;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
-
